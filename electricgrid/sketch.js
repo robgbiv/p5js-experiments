aa21f9e7557a13467f92aee1c0e540e70b6513eb
Robbie R <robbie@roygbiv.co.uk>
2018-08-24 07:48:24 +0100

    Use Shiffman's random walker as an example
    https://github.com/shiffman/The-Nature-of-Code-Examples-p5.js

--- electricgrid/sketch.js
+++ electricgrid/sketch.js
@@ -1,3 +1,5 @@
+let walker;
+
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   background(40, 44, 52);
@@ -13,8 +15,41 @@ function setup() {
       rect(i * gridGap, j * gridGap, gridSize, gridSize);
     }
   }
+  walker = new Walker();
 }
 
 function draw() {
-  // put drawing code here
+  walker.step();
+  walker.render();
+}
+
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+
+  render() {
+    // stroke(255);
+    // point(this.x, this.y);
+    fill(255);
+    rect(this.x, this.y, 10, 10);
+  }
+
+  step() {
+    let choice = floor(random(4));
+    let r = random(1);
+    // A 40% of moving to the right!
+    if (r < 0.4) {
+      this.x += 10;
+    } else if (r < 0.6) {
+      this.x -= 10;
+    } else if (r < 0.8) {
+      this.y += 10;
+    } else {
+      this.y -= 10;
+    }
+    this.x = constrain(this.x, 0, width - 1);
+    this.y = constrain(this.y, 0, height - 1);
+  }
 }
