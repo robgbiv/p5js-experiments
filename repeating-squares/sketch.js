a06c18ea85fc5458448660cc7ee5378e9137c268
Robbie R <robbie@roygbiv.co.uk>
2018-09-30 17:36:24 +0100

    Implement basic rotation

--- repeating-squares/sketch.js
+++ repeating-squares/sketch.js
@@ -7,13 +7,14 @@ function setup() {
   const c = createCanvas(windowWidth, windowHeight);
   background(40, 44, 52);
   angleMode(DEGREES);
+  rectMode(CENTER);
 
   const center = { x: windowWidth / 2, y: windowHeight / 2 };
   RECTSIZE = windowWidth / 4;
 
   startPosition = {
-    x: center.x - (RECTSIZE / 2),
-    y: center.y - (RECTSIZE / 2),
+    x: center.x,
+    y: center.y,
   }
 
   for (var i = 0; i < 4; i++) {
@@ -31,11 +32,11 @@ function oneSide(startPosition, firstRectSize, iteration) {
     allRects.push(new oneRect(centerX, centerY, thisRectSize, lineColour));
     thisRectSize = thisRectSize / 2;
     iteration % 2 === 0
-      ? centerX -= (thisRectSize / 2)
-      : centerX += thisRectSize + (thisRectSize / 2);
+      ? centerX -= thisRectSize
+      : centerX += thisRectSize;
     iteration <= 1
-      ? centerY -= (thisRectSize / 2)
-      : centerY += thisRectSize + (thisRectSize / 2);
+      ? centerY -= thisRectSize
+      : centerY += thisRectSize;
   }
 }
 
@@ -45,24 +46,23 @@ function oneRect(x, y, size, lineColour) {
   this.size = size;
   this.lineColour = lineColour;
   this.angle = 0;
-  stroke(this.lineColour);
-  noFill();
 
   this.display = function() {
-    rect(this.x, this.y, this.size, this.size);
-  }
-
-  this.rotateRect = function() {
+    push();
     translate(this.x, this.y);
+    stroke(this.lineColour);
+    noFill();
+    rect(0, 0, this.size, this.size);
     rotate(this.angle);
-    this.angle += 1;
+    rect(0, 0, this.size, this.size);
+    pop();
+    this.angle += 0.05;
   }
 }
 
 function draw() {
-  let totalRects = allRects.length;
   for (let i = 0; i < allRects.length; i++) {
-    allRects[i].rotateRect();
+    allRects[i].display();
     allRects[i].display();
   }
 }
