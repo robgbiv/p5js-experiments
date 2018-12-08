2d8e4a4d71195a3bea0b21134429826f0d8134fa
Robbie R <robbie@roygbiv.co.uk>
2018-12-08 08:18:12 +0000

    Show lines and dots around circle

---
+++ circle-dots/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  const CENTER = [windowWidth / 2, windowHeight / 2];
+  angleMode(DEGREES);
+  colorMode(HSB, 100);
+  background(0);
+  frameRate(2);
+}
+
+function draw() {
+  // background(0);
+  // stroke(255);
+  // noFill()
+
+  // beginShape();
+
+  // let spacing = map(mouseX, 0, width, 0, 100);
+
+  // endShape(CLOSE);
+  // background(0);
+  drawRing(random(500));
+}
+
+function drawRing(size) {
+  this.colour = fill(random(100), random(100), random(100));
+  const halfWidth = windowWidth / 2;
+  const halfHeight = windowHeight / 2;
+  beginShape();
+  for (let a = 0; a < 360; a += 10) {
+    let x = size * sin(a) + halfWidth - random(10);
+    let y = size * cos(a) + halfHeight + random(10);
+    fill(random(100), random(100), random(100));
+    ellipse(x, y, 3);
+    vertex(x, y);
+    noFill();
+    stroke(255);
+  }
+  endShape(CLOSE);
+
+}
