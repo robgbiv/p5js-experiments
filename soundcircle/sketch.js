5b83bcbbfcbbc7b60538c4c1c0fe0750a8804d80
Robbie R <robbie@roygbiv.co.uk>
2018-12-21 12:45:56 +0000

    Give points movement

--- soundcircle/sketch.js
+++ soundcircle/sketch.js
@@ -1,21 +1,23 @@
 let h, s, b, circleSize, ellipseSize;
 
+let rings = [];
+let points = [];
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     const CENTER = [windowWidth / 2, windowHeight / 2];
     angleMode(DEGREES);
     colorMode(HSB, 100);
     background(0);
-    frameRate(24);
     h = random(100);
     s = 70;
     b = 70;
     circleSize = 1;
     ellipseSize = 1;
-    ring = new Ring();
-    point = new Point();
-    for (let i = 0; i < 200; i++) {
-  	    ring.display(circleSize);
+    for (let i = 0; i < 15; i++) {
+        ring = new Ring();
+        rings.push(ring);
+  	    ring.display(circleSize, h);
         circleSize += 20;
         ellipseSize += 0.1;
         h = (h + 2) % 100;
@@ -23,46 +25,49 @@ function setup() {
 }
 
 function draw() {
-    // background(0);
-    // ring.display(circleSize);
-    point.move();
-    point.display();
+    background(0);
+    points.forEach(point => {
+        point.move();
+        point.display();
+    })
 }
 
 class Ring {
-    constructor() {
+    constructor(hue) {
         this.halfWidth = windowWidth / 2;
         this.halfHeight = windowHeight / 2;
+        this.hue = hue;
     }
 
-    display(size) {
+    display(size, hue) {
         this.size = size;
-        noStroke();
-        fill(h, s, b);
         for (let a = 0; a < 360; a++) {
-            let x = this.size * sin(a) + this.halfWidth - random(5);
-            let y = this.size * cos(a) + this.halfHeight + random(10);
-            point = new Point(x, y, ellipseSize);
+            let x = this.size * sin(a) + this.halfWidth;
+            let y = this.size * cos(a) + this.halfHeight;
+            point = new Point(x, y, ellipseSize, hue);
             point.display();
-            // point.move();
+            points.push(point);
         }
     }
 }
 
 class Point {
-    constructor(x, y, size) {
+    constructor(x, y, size, hue) {
         this.x = x;
         this.y = y;
         this.size = size;
+        this.hue = hue;
         this.speed = 1;
     }
 
     display() {
+        noStroke();
+        fill(this.hue, s, b);
         ellipse(this.x, this.y, this.size);
     }
 
     move() {
-        this.x += (-this.speed, this.speed);
-        this.y += (-this.speed, this.speed);
+        this.x += random(-this.speed, this.speed);
+        this.y += random(-this.speed, this.speed);
     }
 }
