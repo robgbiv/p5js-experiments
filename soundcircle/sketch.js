458224be11c5071de520cb5370814fab74e32138
Robbie R <robbie@roygbiv.co.uk>
2018-12-21 10:29:59 +0000

    Use classes

--- soundcircle/sketch.js
+++ soundcircle/sketch.js
@@ -12,8 +12,10 @@ function setup() {
     b = 70;
     circleSize = 1;
     ellipseSize = 1;
+    ring = new Ring();
+    point = new Point();
     for (let i = 0; i < 200; i++) {
-  	    drawRing(circleSize);
+  	    ring.display(circleSize);
         circleSize += 20;
         ellipseSize += 0.1;
         h = (h + 2) % 100;
@@ -21,16 +23,46 @@ function setup() {
 }
 
 function draw() {
+    // background(0);
+    // ring.display(circleSize);
+    point.move();
+    point.display();
 }
 
-function drawRing(size) {
-    const halfWidth = windowWidth / 2;
-    const halfHeight = windowHeight / 2;
-	  noStroke();
-	  fill(h, s, b);
-    for (let a = 0; a < 360; a++) {
-        let x = size * sin(a) + halfWidth - random(5);
-        let y = size * cos(a) + halfHeight + random(10);
-        ellipse(x, y, ellipseSize);
+class Ring {
+    constructor() {
+        this.halfWidth = windowWidth / 2;
+        this.halfHeight = windowHeight / 2;
+    }
+
+    display(size) {
+        this.size = size;
+        noStroke();
+        fill(h, s, b);
+        for (let a = 0; a < 360; a++) {
+            let x = this.size * sin(a) + this.halfWidth - random(5);
+            let y = this.size * cos(a) + this.halfHeight + random(10);
+            point = new Point(x, y, ellipseSize);
+            point.display();
+            // point.move();
+        }
+    }
+}
+
+class Point {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.speed = 1;
+    }
+
+    display() {
+        ellipse(this.x, this.y, this.size);
+    }
+
+    move() {
+        this.x += (-this.speed, this.speed);
+        this.y += (-this.speed, this.speed);
     }
 }
