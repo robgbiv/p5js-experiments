a20951c4d43fa8eea5e935fa0e777af8d901a2a7
Robbie R <robbie@roygbiv.co.uk>
2019-03-09 14:03:56 +0000

    Remove commented lines
    These were mostly for setting up CCapture to create a gif

--- repeating-squares/sketch.js
+++ repeating-squares/sketch.js
@@ -1,10 +1,3 @@
-var capturer = new CCapture({
-  format: 'gif',
-  name: 'repeatingsquares',
-  workersPath: './ccapture.js/src/',
-});
-var canvas;
-
 const REPETITIONS = 4;
 let RECTSIZE;
 const allRects = [];
@@ -12,31 +5,24 @@ const loopFrame = 288;
 
 function setup() {
   const c = createCanvas(windowWidth, windowHeight);
-  // const canvasSize = 512;
-  // const c = createCanvas(canvasSize, canvasSize);
   canvas = c.canvas;
 
-  // background(40, 44, 52);
   colorMode(HSB, 360, 100, 100);
   angleMode(DEGREES);
   rectMode(CENTER);
   background(220, 23, 20);
 
-  // const center = { x: canvasSize / 2, y: canvasSize / 2 };
-  // RECTSIZE = canvasSize / 4;
   const center = { x: windowWidth / 2, y: windowHeight / 2 };
   RECTSIZE = windowWidth / 4;
 
   const startPosition = {
     x: center.x,
     y: center.y,
-  }
+  };
 
   for (var i = 0; i < 4; i++) {
     oneSide(startPosition, RECTSIZE, i);
   }
-
-  // capturer.start();
 }
 
 function oneSide(startPosition, firstRectSize, iteration) {
@@ -45,19 +31,17 @@ function oneSide(startPosition, firstRectSize, iteration) {
   let thisRectSize = firstRectSize;
   let lineColour = 255;
   let fillColour = color(random(0, 360), 100, 100);
-  // let fillColour = (random(0, 360), random(0, 255), random(0, 255));
   let rotateLeft = false;
 
   for (let i = 0; i < REPETITIONS; i++) {
-    // allRects.push(new oneRect(centerX, centerY, thisRectSize, lineColour, rotateLeft));
-    allRects.push(new oneRect(centerX, centerY, thisRectSize, fillColour, rotateLeft));
+    allRects.push(
+      new oneRect(centerX, centerY, thisRectSize, fillColour, rotateLeft)
+    );
     thisRectSize = thisRectSize / 2;
     iteration % 2 === 0
-      ? (centerX -= thisRectSize, rotateLeft = true)
-      : (centerX += thisRectSize, rotateLeft = false);
-    iteration <= 1
-      ? centerY -= thisRectSize
-      : centerY += thisRectSize;
+      ? ((centerX -= thisRectSize), (rotateLeft = true))
+      : ((centerX += thisRectSize), (rotateLeft = false));
+    iteration <= 1 ? (centerY -= thisRectSize) : (centerY += thisRectSize);
   }
 }
 
@@ -69,12 +53,9 @@ function oneRect(x, y, size, fillColour, rotateLeft) {
   this.angle = 0;
   this.rotateLeft = rotateLeft;
 
-
   this.display = function() {
     push();
     translate(this.x, this.y);
-    // stroke(this.lineColour);
-    // noFill();
     noStroke();
     fill(this.fillColour);
     rect(0, 0, this.size, this.size);
@@ -82,32 +63,17 @@ function oneRect(x, y, size, fillColour, rotateLeft) {
     rect(0, 0, this.size, this.size);
     pop();
     let rotateAngle = 100;
-    // if (this.fillColour <= 127) {
-    //   this.fillColour += 1
-    // } else {
-    //   this.fillColour -= 1;
-    // }
     if (this.rotateLeft) {
       this.angle -= rotateAngle / this.size;
     } else {
       this.angle += rotateAngle / this.size;
     }
-  }
+  };
 }
 
 function draw() {
-  // background(40, 44, 52);
-   // if (frameCount % 60 === 0) {
-   //   background(220, 23, 20);
-  // }
   background(220, 23, 20);
   for (let i = 0; i < allRects.length; i++) {
     allRects[i].display();
   }
-  // if (frameCount < loopFrame) {
-  //   capturer.capture(canvas);
-  // } else if (frameCount === loopFrame) {
-  //   capturer.stop();
-  //   capturer.save();
-  // }
 }
