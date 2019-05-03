8043a8635b46b21f0d9eb3442169afdb1fed0231
Robbie R <robbie@faculty.ai>
2019-05-03 10:47:59 +0100

    Use colours from photo of piece

--- orion-mc/sketch.js
+++ orion-mc/sketch.js
@@ -6,26 +6,55 @@ let frameSize, tileSize, tileWidth;
 let tiles = [];
 let shapes = [];
 
-const tileGradient = [
-  [32, 32, 32],
-  [64, 64, 64],
-  [96, 96, 96],
-  [128, 128, 128],
-  [160, 160, 160],
-  [192, 192, 192],
-  [224, 224, 224],
-  [255, 255, 255]
-];
+// const tileGradient = [
+//   [32, 32, 32],
+//   [64, 64, 64],
+//   [96, 96, 96],
+//   [128, 128, 128],
+//   [160, 160, 160],
+//   [192, 192, 192],
+//   [224, 224, 224],
+//   [255, 255, 255]
+// ];
 
-const shapeColours = [
-  [148, 0, 211],
-  [75, 0, 130],
-  [0, 0, 255],
-  [0, 255, 0],
-  [255, 255, 0],
-  [255, 127, 0],
-  [255, 0, 0]
-];
+// const shapeColours = [
+//   [148, 0, 211],
+//   [75, 0, 130],
+//   [0, 0, 255],
+//   [0, 255, 0],
+//   [255, 255, 0],
+//   [255, 127, 0],
+//   [255, 0, 0]
+// ];
+
+const tileGradient = [
+  [40, 35, 30],
+  [61, 57, 51],
+  [100, 92, 86],
+  [129, 119, 113],
+  [153, 142, 131],
+  [183, 170, 154],
+  [209, 195, 174],
+  [227, 211, 187],
+]
+
+const shapeColours = {
+  "darkblue": [0, 59, 89],
+  "blue": [35, 111, 167],
+  "lightblue": [54, 132, 195],
+  "darkgreen": [58, 78, 48],
+  "midgreen": [66, 133, 76],
+  "lightgreen": [46, 197, 95],
+  "orange": [255, 110, 8],
+  "lightorange": [255, 161, 0],
+  "yellow": [255, 220, 0],
+  "darkred": [172, 62, 51],
+  "midred": [221, 61, 36],
+  "red": [255, 60, 30],
+  "darkpurple": [128, 56, 91],
+  "purple": [121, 76, 126],
+
+}
 
 function setup() {
   const c = createCanvas(windowWidth, windowHeight);
@@ -62,9 +91,9 @@ function setup() {
 
   for (let i = 0; i < tilesPerSide; i++) {
     for (let j = 0; j < tilesPerSide; j++) {
-      let tileColour = tileGradient[(i + j) % tileGradient.length];
-      // let tileColour =
-      //   tileGradient[Math.floor(Math.random() * tileGradient.length)];
+      // let tileColour = tileGradient[(i + j) % tileGradient.length];
+      let tileColour =
+        tileGradient[Math.floor(Math.random() * tileGradient.length)];
       let tile = new Tile(
         i * tileSize + frameStartX,
         j * tileSize + frameStartY,
@@ -94,8 +123,8 @@ class Tile {
     let shapeX = this.x + this.size / 2;
     let shapeY = this.y + this.size / 2;
     let shapeSize = this.size - Math.floor(Math.random() * 40);
-    let shapeColour =
-      shapeColours[Math.floor(Math.random() * shapeColours.length)];
+    let keys = Object.keys(shapeColours)
+    let shapeColour = shapeColours[keys[ keys.length * Math.random() << 0]];
     let shape = new Shape(shapeX, shapeY, shapeSize, shapeColour);
     shapes.push(shape);
     shape.display();
